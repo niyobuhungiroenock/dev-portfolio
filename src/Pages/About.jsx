ad80bc33a65fb674aa50e942ef69b1fdd89a59c2
buhungiro <enocniyobuhuro22@email.com>
2026-01-07 16:56:43 +0200

    my first  real comment

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold mb-4">About Me</h1>
-      <p>I’m a passionate React developer learning full-stack development!</p>
+      <p>I’m a passionate React developer learning full-stack development! I'm learning to use git and GitHub on my projects.</p>
     </div>
   )
 }
